27226f151a0dff2a38d8ede36b9b526f97a67715
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2020-05-11 17:21:34 +0530

    images bug fix 1

--- src/images.js
+++ src/images.js
@@ -1,14 +1,14 @@
 const images = [
-    {id: 1, src: "/assets/blobs/blobs1.png", title: "Blobs", description: "blobs image"},
-    {id: 2, src: "/assets/blobs/eyes.gif", title: "Blobs", description: "blobs image"},
-    {id: 3, src: "/assets/blobs/blobs3.png", title: "Blobs", description: "blobs image"},
-    {id: 4, src: "/assets/blobs/blobs2.png", title: "Blobs", description: "blobs image"},
-    {id: 5, src: "/assets/blobs/leaves.gif", title: "Blobs", description: "blobs image"},
-    {id: 6, src: "/assets/blobs/waves.gif", title: "Blobs", description: "blobs image"},
-    {id: 7, src: "/assets/blobs/leaves.gif", title: "Blobs", description: "blobs image"},
-    {id: 8, src: "/assets/blobs/waves1.gif", title: "Blobs", description: "blobs image"},
-    {id: 9, src: "/assets/blobs/stairs.png", title: "Blobs", description: "blobs image"},
-    {id: 10, src: "/assets/blobs/style_transfer.jpeg", title: "Blobs", description: "blobs image"}
+    {id: 1, src: process.env.PUBLIC_URL + "/assets/blobs/blobs1.png", title: "Blobs", description: "blobs image"},
+    {id: 2, src: process.env.PUBLIC_URL + "/assets/blobs/eyes.gif", title: "Blobs", description: "blobs image"},
+    {id: 3, src: process.env.PUBLIC_URL + "/assets/blobs/blobs3.png", title: "Blobs", description: "blobs image"},
+    {id: 4, src: process.env.PUBLIC_URL + "/assets/blobs/blobs2.png", title: "Blobs", description: "blobs image"},
+    {id: 5, src: process.env.PUBLIC_URL + "/assets/blobs/leaves.gif", title: "Blobs", description: "blobs image"},
+    {id: 6, src: process.env.PUBLIC_URL + "/assets/blobs/waves.gif", title: "Blobs", description: "blobs image"},
+    {id: 7, src: process.env.PUBLIC_URL + "/assets/blobs/leaves.gif", title: "Blobs", description: "blobs image"},
+    {id: 8, src: process.env.PUBLIC_URL + "/assets/blobs/waves1.gif", title: "Blobs", description: "blobs image"},
+    {id: 9, src: process.env.PUBLIC_URL + "/assets/blobs/stairs.png", title: "Blobs", description: "blobs image"},
+    {id: 10, src: process.env.PUBLIC_URL + "/assets/blobs/style_transfer.jpeg", title: "Blobs", description: "blobs image"}
 
 ]
 
