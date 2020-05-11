0fc8afa208e4eaa1fa274e5af8294fba718b894c
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2020-05-11 17:13:26 +0530

    Updates

---
+++ precache-manifest.7da14f2424f0dd427c98c29bf9e6c5a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70e862a85bf5cddf55caf96668933ab0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "af73968693f712db51d6",
+    "url": "/portfolio/static/css/2.e2b18e3b.chunk.css"
+  },
+  {
+    "revision": "668d3e5aaec7328ccf06",
+    "url": "/portfolio/static/css/main.4ca28209.chunk.css"
+  },
+  {
+    "revision": "af73968693f712db51d6",
+    "url": "/portfolio/static/js/2.97e0e2a1.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/portfolio/static/js/2.97e0e2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "668d3e5aaec7328ccf06",
+    "url": "/portfolio/static/js/main.a8b2c12f.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "12469cd741ee0b0a3fe13511550c4e24",
+    "url": "/portfolio/static/media/JustSignature-92w7.12469cd7.ttf"
+  }
+]);
